cc1ab34ac07f9ef43a9ebb7c23808846e132c5d0
Alberto Romero <aromeronavia@gmail.com>
2021-09-23 01:50:17 -0500

    Add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,36 @@
 <template>
   <div>
+    <nav class="navbar">
+      <p class="explore-the-freat"> Explore the freat </p>
+      <p>
+        <a class="github" href="https://github.com/beeetooo/guitar-scales" target="_blank"> Github </a>
+      </p>
+    </nav>
     <Nuxt />
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  font-family: Poppins;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
+body {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+.navbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 24px 0 24px;
+  background-color: #7692ff;
+}
+
+.github {
   text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  color: black;
 }
 </style>
