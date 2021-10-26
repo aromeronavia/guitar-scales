f815727c055bb23e3434598da3411afacae3a28e
Alberto Romero <aromeronavia@gmail.com>
2021-10-25 23:30:40 -0500

    Apply prettier to js files

--- constants.js
+++ constants.js
@@ -1,18 +1,31 @@
 export const A4 = {
-  FREQUENCY: 440.00,
-  NOTE_NAME: "A",
-  OCTAVE: 4,
-};
+  FREQUENCY: 440.0,
+  NOTE_NAME: 'A',
+  OCTAVE: 4
+}
 
-export const tones = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+export const tones = [
+  'C',
+  'C#',
+  'D',
+  'D#',
+  'E',
+  'F',
+  'F#',
+  'G',
+  'G#',
+  'A',
+  'A#',
+  'B'
+]
 
-const ionian =      [2, 2, 1, 2, 2, 2, 1];
-const dorian =      [2, 1, 2, 2, 2, 1, 2];
-const phrygian =    [1, 2, 2, 2, 1, 2, 2];
-const lydian =      [2, 2, 2, 1, 2, 2, 1];
-const mixolydian =  [2, 2, 1, 2, 2, 1, 2];
-const aeolian =     [2, 1, 2, 2, 1, 2, 2];
-const locrian =     [1, 2, 2, 1, 2, 2, 2];
+const ionian = [2, 2, 1, 2, 2, 2, 1]
+const dorian = [2, 1, 2, 2, 2, 1, 2]
+const phrygian = [1, 2, 2, 2, 1, 2, 2]
+const lydian = [2, 2, 2, 1, 2, 2, 1]
+const mixolydian = [2, 2, 1, 2, 2, 1, 2]
+const aeolian = [2, 1, 2, 2, 1, 2, 2]
+const locrian = [1, 2, 2, 1, 2, 2, 2]
 
 export const scales = {
   ionian,
@@ -22,19 +35,19 @@ export const scales = {
   mixolydian,
   aeolian,
   locrian
-};
+}
 
-const major =           [4, 3];
-const major7 =          [4, 3, 4];
-const major9 =          [4, 3, 4, 3];
-const major11 =         [4, 3, 4, 3, 3];
-const dominant7 =       [4, 3, 3];
-const diminished =      [3, 3, 3];
-const halfDiminished =  [3, 3, 4];
-const minor =           [3, 4];
-const minor7 =          [3, 4, 3];
-const minor9 =          [3, 4, 3, 4];
-const minor11 =         [3, 4, 3, 4, 3];
+const major = [4, 3]
+const major7 = [4, 3, 4]
+const major9 = [4, 3, 4, 3]
+const major11 = [4, 3, 4, 3, 3]
+const dominant7 = [4, 3, 3]
+const diminished = [3, 3, 3]
+const halfDiminished = [3, 3, 4]
+const minor = [3, 4]
+const minor7 = [3, 4, 3]
+const minor9 = [3, 4, 3, 4]
+const minor11 = [3, 4, 3, 4, 3]
 
 export const chords = {
   major,
@@ -47,10 +60,10 @@ export const chords = {
   minor,
   minor7,
   minor9,
-  minor11,
-};
+  minor11
+}
 
 export const DISPLAY_MODES = {
-  CHORD: "CHORD",
-  SCALE: "SCALE",
-};
+  CHORD: 'CHORD',
+  SCALE: 'SCALE'
+}
--- engine/string.js
+++ engine/string.js
@@ -1,38 +1,41 @@
-import { tones, scales, chords, DISPLAY_MODES } from '@/constants';
-import Note from '@/engine/note';
+import { tones, scales, chords, DISPLAY_MODES } from '@/constants'
+import Note from '@/engine/note'
 
-const NUMBER_OF_NOTES = 12;
-const NUMBER_OF_FRETS = 13;
+const NUMBER_OF_NOTES = 12
+const NUMBER_OF_FRETS = 13
 
 export default class StringBuilder {
   build(note) {
-    const tonicIndex = tones.indexOf(note.noteName);
+    const tonicIndex = tones.indexOf(note.noteName)
 
-    const stringNotes = [note];
+    const stringNotes = [note]
     for (let i = 1; i < NUMBER_OF_FRETS; i++) {
-      const noteName = tones[(tonicIndex + i) % NUMBER_OF_NOTES];
-      let octave = note.octave + (tones.indexOf(noteName) <= tonicIndex  ? 1 : 0);
+      const noteName = tones[(tonicIndex + i) % NUMBER_OF_NOTES]
+      let octave = note.octave + (tones.indexOf(noteName) <= tonicIndex ? 1 : 0)
       stringNotes.push(
         new Note({
           noteName,
-          octave,
-      }));
+          octave
+        })
+      )
     }
 
-    return stringNotes;
+    return stringNotes
   }
   getNotes(scaleName, tone) {
-    const noteDistances = scales[scaleName];
-    const currentToneIndex = tones.indexOf(tone);
-    const tonesStartingInSelected = tones.map((_, i) => tones[(currentToneIndex + i) % tones.length]);
+    const noteDistances = scales[scaleName]
+    const currentToneIndex = tones.indexOf(tone)
+    const tonesStartingInSelected = tones.map(
+      (_, i) => tones[(currentToneIndex + i) % tones.length]
+    )
 
-    const notes = [tonesStartingInSelected[0]];
-    let cursor = 0;
+    const notes = [tonesStartingInSelected[0]]
+    let cursor = 0
     noteDistances.forEach((distance) => {
-      cursor = (cursor + distance) % tonesStartingInSelected.length;
-      notes.push(tonesStartingInSelected[cursor]);
-    });
+      cursor = (cursor + distance) % tonesStartingInSelected.length
+      notes.push(tonesStartingInSelected[cursor])
+    })
 
-    return notes;
+    return notes
   }
 }
